47b228f3a1438ff1cbb859d21baf3ce1d45db494
tonynokart <thomasson5leo@gmail.com>
2023-11-03 02:02:55 -0400

    menu + arrow1 works, javascript in progress

--- java.js
+++ java.js
@@ -1,9 +1,30 @@
-//for later
+//work in progress
 
-function ClickMenu() {
-    var tag = document.getElementById("menu");
-    tag.style.transition= "transform .5s ease-in-out";
-    tag.style.transform = "translateX(0%)";
+const menu = document.querySelector('.menu');
+const close = document.querySelector('.close');
+const nav = document.querySelector('nav');
+
+menu.addEventListener('click', () => {
+    nav.classList.add('open-nav')
+})
+close.addEventListener('click', () => {
+    nav.classList.remove('open-nav')
+})
+
+const arrow1 = document.querySelector('.arrow.one');
+const arrow2 = document.querySelector('.arrow.two')
+
+//when arrow1 is clicked the screen scrolls down
+arrow1.addEventListener('click', () => {
+    window.scrollTo(0, 740) //needs fixing, moves to px coordinates not with screen size
+})
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.querySelector("header").style.background = "white";
+  } else {
+    document.querySelector("header").style.background = "unset";
+  }
 }
-
-document.getElementById('myButton').onclick = ClickMenu;
